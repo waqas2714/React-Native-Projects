c3895e28dddc09c253a81e575e30f6256d5b3a24
waqas2714 <waqasali00123@gmail.com>
2024-05-08 23:25:05 +0500

    started the third native project after fixing android sdk

---
+++ Third-Native-Project/components/CategoryGridTile.js
@@ -0,0 +1,48 @@
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native"
+
+const CategoryGridTile = ({title, color, onPress}) => {
+  return (
+    <View style={[styles.gridItem, { backgroundColor : color }]}>
+        <Pressable onPress={onPress} style={({pressed})=>[styles.button, pressed ? styles.buttonPressed : null]} android_ripple={{color : 'ccc'}}>
+            <View style={styles.innerContainer}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        </Pressable>
+    </View>
+  )
+}
+
+export default CategoryGridTile
+
+const styles = StyleSheet.create({
+    gridItem : {
+      flex : 1,
+      margin : 16,
+      height : 150,
+      borderRadius : 8,
+      elevation : 4,
+      backgroundColor : 'white',
+      shadowColor : 'black',
+      shadowOpacity : 0.25,
+      shadowOffset : {width : 0, height : 2},
+      shadowRadius : 8,
+      overflow : Platform.OS == 'android' ? 'hidden' : 'visible'
+    },
+    button : {
+      flex : 1
+    },
+    buttonPressed : {
+        opacity : 0.25
+    },
+    innerContainer : {
+      flex : 1,
+      padding : 16,
+      justifyContent : 'center',
+      alignItems : 'center'
+    },
+    title : {
+      fontWeight : 'bold',
+      fontSize : 18
+    }
+  });
+  
---
+++ Third-Native-Project/screens/CategoriesScreen.js
@@ -0,0 +1,25 @@
+import { FlatList } from "react-native"
+import { CATEGORIES } from "../data/dummyData"
+import CategoryGridTile from "../components/CategoryGridTile"
+
+
+const CategoriesScreen = ({navigation}) => {
+
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview')
+        }
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />
+    }
+
+  return (
+    <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item)=>item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+    />
+  )
+}
+
+export default CategoriesScreen
